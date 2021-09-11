c495690d2869854bef99956fbef097b6c66169e8
Riko Uchida <rikococouchida2008@gmail.com>
2021-09-11 16:30:57 +0900

    paddleが動くようになったのと、ballが跳ね返るようになりました。

--- src/riko/uchida/Paddle.java
+++ src/riko/uchida/Paddle.java
@@ -2,15 +2,25 @@ package riko.uchida;
 
 import processing.core.PApplet;
 
+import static processing.core.PConstants.RADIUS;
+
 class Paddle {
-    int x;
-    int y;
+    int x = 255;
+    int y = 450;
+    int w = 50;
+    int h = 10;
 
     public void draw(PApplet pApplet) {
+        pApplet.noStroke();
+        pApplet.fill(255,255,255);
+        pApplet.rect(x, y, w, h);
 
     }
 
     public void update(PApplet pApplet) {
-
+        if(pApplet.keyPressed){
+            if (pApplet.keyCode == 37) x -= 5;
+            if(pApplet.keyCode == 39) x += 5;
+        }
     }
 }
--- src/riko/uchida/Stage.java
+++ src/riko/uchida/Stage.java
@@ -12,6 +12,18 @@ class Stage {
     public void update(PApplet pApplet) {
         ball.update(pApplet);
         paddle.update(pApplet);
+        int bx = ball.x + ball.dx;
+        int by = ball.y + ball.dy;
+
+        int px = paddle.x;
+        int py = paddle.y;
+        int pw = paddle.w;
+        int ph = paddle.h;
+        //px < bx < (px+pw)
+        //py < by < (py+ph)
+        if(px < bx && bx < (px+pw) && py < by && py< (py+ph)){
+            ball.dy = ball.dy*-1;
+        }
     }
 
     public void draw(PApplet pApplet) {
