2a9d617aa5783e7c884377a4f16eb9fba75ec97d
Riko Uchida <rikococouchida2008@gmail.com>
2021-10-16 15:05:12 +0900

    blockを並べました

--- src/riko/uchida/Block.java
+++ src/riko/uchida/Block.java
@@ -2,15 +2,35 @@ package riko.uchida;
 
 import processing.core.PApplet;
 
-public class Block {
+public class Block implements Collision{
+    int bx;
+    int by;
+    static int WIDTH = 30;
+    static int HEIGHT = 15;
+    public Block(int x, int y){
+        bx= x;
+        by = y;
+    }
+
     public void draw(PApplet pApplet) {
         pApplet.noStroke();
         pApplet.fill(255,255,255);
-        pApplet.rect(50,50,50,50);
+        pApplet.rect(bx, by, WIDTH, HEIGHT);
 
     }
 
-    public void update(PApplet pApplet) {
+
+    @Override
+    public boolean isHit(int x, int y) {
+        if(this.bx<x && x<(this.bx + WIDTH)&&this.by<y&&y<(this.by+HEIGHT)){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onAction(int x, int y) {
 
     }
+
 }
